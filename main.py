493531741e18f467e39458a524d2f8f7768706ea
nagima Dubanaeva <98920760+nagimad@users.noreply.github.com>
2022-02-15 01:04:26 +0000

    added info based on the instructions

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
+classes_per_week = 3
+cost_per_class =  (cost_per_week/classes_per_week)
+print("Cost per class:",cost_per_class)
+print("message - how was your weekend?:))")
+print(weeks,type(weeks))
+print(classes,type(classes))
+print(tuition,type(tuition))
+print(cost_per_week,type(cost_per_week))
+print(classes_per_week, type(classes_per_week))
+print(cost_per_class, type(cost_per_class))
+
 
 
 #Part B
